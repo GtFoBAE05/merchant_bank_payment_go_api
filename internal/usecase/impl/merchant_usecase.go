8e27ac33fe6c6335024666137d8ebe1ea9d51858
GtFoBAE05 <y.imannuel@gmail.com>
2024-11-23 22:35:21 +0700

    feat: add merchant use case implementation

--- internal/usecase/impl/merchant_usecase.go
+++ internal/usecase/impl/merchant_usecase.go
@@ -1,23 +1,35 @@
 package impl
 
 import (
+	"github.com/google/uuid"
 	"github.com/sirupsen/logrus"
 	"merchant_bank_payment_go_api/internal/entity"
 	"merchant_bank_payment_go_api/internal/repository"
 )
 
 type MerchantUseCaseImpl struct {
-	log                *logrus.Logger
+	Log                *logrus.Logger
 	MerchantRepository repository.MerchantRepository
 }
 
 func NewMerchantUseCaseImpl(log *logrus.Logger, merchantRepository repository.MerchantRepository) *MerchantUseCaseImpl {
 	return &MerchantUseCaseImpl{
-		log:                log,
+		Log:                log,
 		MerchantRepository: merchantRepository,
 	}
 }
 
-func (m MerchantUseCaseImpl) FindById(id string) (entity.Merchant, error) {
-	return entity.Merchant{}, nil
+func (m *MerchantUseCaseImpl) FindById(id string) (entity.Merchant, error) {
+	parsedUUID, err := uuid.Parse(id)
+	if err != nil {
+		m.Log.Errorf("Failed to parse uuid: %s", id)
+		return entity.Merchant{}, err
+	}
+	merchant, err := m.MerchantRepository.FindById(parsedUUID)
+	if err != nil {
+		m.Log.Errorf("Failed to find merchant with id %s: %v", id, err)
+		return entity.Merchant{}, err
+	}
+
+	return merchant, nil
 }
