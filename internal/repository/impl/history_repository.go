8e791892ab80f6eb38dd93f06c04fa2aaae99409
GtFoBAE05 <y.imannuel@gmail.com>
2024-11-24 00:23:09 +0700

    feat: add history repository implementation

--- internal/repository/impl/history_repository.go
+++ internal/repository/impl/history_repository.go
@@ -1,31 +1,69 @@
 package impl
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/sirupsen/logrus"
 	"merchant_bank_payment_go_api/internal/entity"
+	"merchant_bank_payment_go_api/internal/utils"
 )
 
 type HistoryRepositoryImpl struct {
 	Log      *logrus.Logger
-	filename string
+	Filename string
 }
 
 func NewHistoryRepositoryImpl(log *logrus.Logger, filename string) *HistoryRepositoryImpl {
 	return &HistoryRepositoryImpl{
 		Log:      log,
-		filename: filename,
+		Filename: filename,
 	}
 }
 
 func (h *HistoryRepositoryImpl) LoadHistories() ([]entity.History, error) {
-	return nil, nil
+	h.Log.Debugf("Loading histories from file: %s", h.Filename)
+
+	file, err := utils.ReadJsonFile(h.Filename, h.Log)
+	if err != nil {
+		h.Log.Errorf("Error reading file %s: %v", h.Filename, err)
+		return nil, err
+	}
+
+	h.Log.Tracef("File content: %s", string(file))
+
+	var histories []entity.History
+	err = json.Unmarshal(file, &histories)
+	if err != nil {
+		h.Log.Errorf("Error decoding JSON from file %s: %v", h.Filename, err)
+		return nil, err
+	}
+
+	h.Log.Infof("Successfully loaded %d histories from %s", len(histories), h.Filename)
+	return histories, nil
 }
 
 func (h *HistoryRepositoryImpl) SaveHistories(histories []entity.History) error {
-	return nil
+	h.Log.Infof("Saving %d histories to file: %s", len(histories), h.Filename)
 
+	err := utils.WriteJSONFile(h.Filename, histories, h.Log)
+	if err != nil {
+		h.Log.Errorf("Error saving histories to file %s: %v", h.Filename, err)
+		return fmt.Errorf("error saving histories to file %s: %v", h.Filename, err)
+	}
+
+	h.Log.Infof("Successfully saved histories to %s", h.Filename)
+	return nil
 }
 
 func (h *HistoryRepositoryImpl) AddHistory(history entity.History) error {
-	return nil
+	histories, err := h.LoadHistories()
+	if err != nil {
+		return err
+	}
+
+	h.Log.Infof("Adding history %s to histories", history)
+
+	histories = append(histories, history)
+
+	return h.SaveHistories(histories)
 }
