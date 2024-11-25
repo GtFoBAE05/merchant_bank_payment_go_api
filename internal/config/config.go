535a074369a1ca5905f0b3bddca4adc787687c3d
GtFoBAE05 <y.imannuel@gmail.com>
2024-11-25 16:53:43 +0700

    refactor: add sample env data and adjust migrated from hardcode secret key to env

---
+++ internal/config/config.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"os"
+	"strconv"
+)
+
+type Config struct {
+	SecretKey       []byte
+	ExpireInMinutes int
+}
+
+func LoadConfig() (*Config, error) {
+	err := godotenv.Load(".env")
+	if err != nil {
+		return nil, fmt.Errorf("Error loading .env file: %v", err)
+	}
+
+	secretKey := os.Getenv("SECRET_KEY")
+	if len(secretKey) == 0 {
+		return nil, fmt.Errorf("SECRET_KEY not set in environment variables")
+	}
+
+	expireInMinutes, err := strconv.Atoi(os.Getenv("EXPIRE_IN_MINUTES"))
+	if err != nil || expireInMinutes <= 0 {
+		return nil, fmt.Errorf("EXPIRE_IN_MINUTES is not set or invalid")
+	}
+
+	return &Config{
+		SecretKey:       []byte(secretKey),
+		ExpireInMinutes: expireInMinutes,
+	}, nil
+}
