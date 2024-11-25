46dde049c79e37c5275e2ab03b54e278e9302289
GtFoBAE05 <y.imannuel@gmail.com>
2024-11-25 11:53:26 +0700

    refactor: refactor customer test case

--- test/repository_test/customer_repository_test.go
+++ test/repository_test/customer_repository_test.go
@@ -7,26 +7,26 @@ import (
 	"github.com/stretchr/testify/assert"
 	"merchant_bank_payment_go_api/internal/entity"
 	"merchant_bank_payment_go_api/internal/repository/impl"
-	"merchant_bank_payment_go_api/test/test_helpers"
+	"merchant_bank_payment_go_api/test/helper"
 	"os"
 	"testing"
 )
 
 func CreateCustomerTempFile() {
-	fileContent, err := json.Marshal(test_helpers.ExpectedCustomers)
+	fileContent, err := json.Marshal(helper.ExpectedCustomers)
 	if err != nil {
 		logrus.Error("Error marshalling data:", err)
 		return
 	}
 
-	err = os.WriteFile(test_helpers.CustomerFilename, fileContent, 0644)
+	err = os.WriteFile(helper.CustomerFilename, fileContent, 0644)
 	if err != nil {
 		logrus.Error("Error writing to file:", err)
 	}
 }
 
 func DeleteCustomerTempfile() {
-	err := os.Remove(test_helpers.CustomerFilename)
+	err := os.Remove(helper.CustomerFilename)
 	if err != nil && !os.IsNotExist(err) {
 		logrus.Error("Error removing file:", err)
 	}
@@ -37,17 +37,17 @@ func TestLoadCustomers_ShouldReturnCustomerList(t *testing.T) {
 	CreateCustomerTempFile()
 
 	log := logrus.New()
-	repo := impl.NewCustomerRepositoryImpl(log, test_helpers.CustomerFilename)
+	repo := impl.NewCustomerRepositoryImpl(log, helper.CustomerFilename)
 
 	customerResults, err := repo.LoadCustomers()
 
 	assert.Nil(t, err)
-	assert.Equal(t, len(test_helpers.ExpectedCustomers), len(customerResults))
-	assert.Equal(t, test_helpers.ExpectedCustomers[0].Id, customerResults[0].Id)
-	assert.Equal(t, test_helpers.ExpectedCustomers[0].Username, customerResults[0].Username)
-	assert.Equal(t, test_helpers.ExpectedCustomers[0].Password, customerResults[0].Password)
-	assert.Equal(t, test_helpers.ExpectedCustomers[0].CreatedAt, customerResults[0].CreatedAt)
-	assert.Equal(t, test_helpers.ExpectedCustomers[0].UpdatedAt, customerResults[0].UpdatedAt)
+	assert.Equal(t, len(helper.ExpectedCustomers), len(customerResults))
+	assert.Equal(t, helper.ExpectedCustomers[0].Id, customerResults[0].Id)
+	assert.Equal(t, helper.ExpectedCustomers[0].Username, customerResults[0].Username)
+	assert.Equal(t, helper.ExpectedCustomers[0].Password, customerResults[0].Password)
+	assert.Equal(t, helper.ExpectedCustomers[0].CreatedAt, customerResults[0].CreatedAt)
+	assert.Equal(t, helper.ExpectedCustomers[0].UpdatedAt, customerResults[0].UpdatedAt)
 }
 
 func TestLoadCustomers_ShouldReturnError_WhenInvalidFile(t *testing.T) {
@@ -63,21 +63,21 @@ func TestLoadCustomers_ShouldReturnError_WhenInvalidFile(t *testing.T) {
 }
 
 func TestLoadCustomers_ShouldReturnError_WhenInvalidContent(t *testing.T) {
-	err := os.WriteFile(test_helpers.CustomerFilename, []byte(""), 0644)
+	err := os.WriteFile(helper.CustomerFilename, []byte(""), 0644)
 	if err != nil {
 		logrus.Error("Error writing to file:", err)
 		return
 	}
 
 	log := logrus.New()
-	repo := impl.NewCustomerRepositoryImpl(log, test_helpers.CustomerFilename)
+	repo := impl.NewCustomerRepositoryImpl(log, helper.CustomerFilename)
 
 	customerResults, err := repo.LoadCustomers()
 
 	assert.Nil(t, customerResults)
 	assert.NotNil(t, err)
 
-	err = os.Remove(test_helpers.CustomerFilename)
+	err = os.Remove(helper.CustomerFilename)
 	if err != nil {
 		logrus.Error("Error deleting to file:", err)
 	}
@@ -88,15 +88,15 @@ func TestFindById_ShouldReturnCustomer(t *testing.T) {
 	CreateCustomerTempFile()
 
 	log := logrus.New()
-	repo := impl.NewCustomerRepositoryImpl(log, test_helpers.CustomerFilename)
+	repo := impl.NewCustomerRepositoryImpl(log, helper.CustomerFilename)
 
-	customerResult, err := repo.FindById(test_helpers.CustomerId)
+	customerResult, err := repo.FindById(helper.CustomerId)
 	assert.Nil(t, err)
-	assert.Equal(t, test_helpers.ExpectedCustomers[0].Id, customerResult.Id)
-	assert.Equal(t, test_helpers.ExpectedCustomers[0].Username, customerResult.Username)
-	assert.Equal(t, test_helpers.ExpectedCustomers[0].Password, customerResult.Password)
-	assert.Equal(t, test_helpers.ExpectedCustomers[0].CreatedAt, customerResult.CreatedAt)
-	assert.Equal(t, test_helpers.ExpectedCustomers[0].UpdatedAt, customerResult.UpdatedAt)
+	assert.Equal(t, helper.ExpectedCustomers[0].Id, customerResult.Id)
+	assert.Equal(t, helper.ExpectedCustomers[0].Username, customerResult.Username)
+	assert.Equal(t, helper.ExpectedCustomers[0].Password, customerResult.Password)
+	assert.Equal(t, helper.ExpectedCustomers[0].CreatedAt, customerResult.CreatedAt)
+	assert.Equal(t, helper.ExpectedCustomers[0].UpdatedAt, customerResult.UpdatedAt)
 }
 
 func TestFindByCustomerId_ShouldReturnError_WhenNotFound(t *testing.T) {
@@ -104,7 +104,7 @@ func TestFindByCustomerId_ShouldReturnError_WhenNotFound(t *testing.T) {
 	CreateCustomerTempFile()
 
 	log := logrus.New()
-	repo := impl.NewCustomerRepositoryImpl(log, test_helpers.CustomerFilename)
+	repo := impl.NewCustomerRepositoryImpl(log, helper.CustomerFilename)
 
 	customerResult, err := repo.FindById(uuid.New())
 	assert.NotNil(t, err)
@@ -125,15 +125,15 @@ func TestFindByUsername_ShouldReturnCustomer(t *testing.T) {
 	CreateCustomerTempFile()
 
 	log := logrus.New()
-	repo := impl.NewCustomerRepositoryImpl(log, test_helpers.CustomerFilename)
+	repo := impl.NewCustomerRepositoryImpl(log, helper.CustomerFilename)
 
-	customerResult, err := repo.FindByUsername(test_helpers.ExpectedCustomers[0].Username)
+	customerResult, err := repo.FindByUsername(helper.ExpectedCustomers[0].Username)
 	assert.Nil(t, err)
-	assert.Equal(t, test_helpers.ExpectedCustomers[0].Id, customerResult.Id)
-	assert.Equal(t, test_helpers.ExpectedCustomers[0].Username, customerResult.Username)
-	assert.Equal(t, test_helpers.ExpectedCustomers[0].Password, customerResult.Password)
-	assert.Equal(t, test_helpers.ExpectedCustomers[0].CreatedAt, customerResult.CreatedAt)
-	assert.Equal(t, test_helpers.ExpectedCustomers[0].UpdatedAt, customerResult.UpdatedAt)
+	assert.Equal(t, helper.ExpectedCustomers[0].Id, customerResult.Id)
+	assert.Equal(t, helper.ExpectedCustomers[0].Username, customerResult.Username)
+	assert.Equal(t, helper.ExpectedCustomers[0].Password, customerResult.Password)
+	assert.Equal(t, helper.ExpectedCustomers[0].CreatedAt, customerResult.CreatedAt)
+	assert.Equal(t, helper.ExpectedCustomers[0].UpdatedAt, customerResult.UpdatedAt)
 }
 
 func TestFindByUsername_ShouldReturnError_WhenNotFound(t *testing.T) {
@@ -141,7 +141,7 @@ func TestFindByUsername_ShouldReturnError_WhenNotFound(t *testing.T) {
 	CreateCustomerTempFile()
 
 	log := logrus.New()
-	repo := impl.NewCustomerRepositoryImpl(log, test_helpers.CustomerFilename)
+	repo := impl.NewCustomerRepositoryImpl(log, helper.CustomerFilename)
 
 	customerResult, err := repo.FindByUsername("aaa")
 	assert.NotNil(t, err)
